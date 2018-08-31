8aac7ec1280f7ef9b0b689584e31ed4fa738f3d3
Anton Developer <tf.2.98@mail.ru>
2018-08-31 06:19:35 +0300

    base project setup

---
+++ TODO Lite/TODO Lite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODO Lite
+//
+//  Created by Anton Millionaire on 8/31/18.
+//  Copyright © 2018 Anton Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
