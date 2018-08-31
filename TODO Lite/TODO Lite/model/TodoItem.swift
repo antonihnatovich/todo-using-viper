85636fa91497d32b9a7207694a9e48a6ae7efbc1
Anton Developer <tf.2.98@mail.ru>
2018-08-31 07:27:36 +0300

    added todoitem model

--- TODO Lite/TODO Lite/model/TodoItem.swift
+++ TODO Lite/TODO Lite/model/TodoItem.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+protocol TodoItemProtocol {
+    var id: Int { get set }
+    var name: String { get set }
+    var category: String { get set }
+    var date: Date { get set }
+    var isComlited: Bool { get set }
+}
+
+struct TodoItem: TodoItemProtocol, Equatable {
+    var id: Int
+    var name: String
+    var category: String
+    var date: Date
+    var isComlited: Bool
+}
