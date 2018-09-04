62faff35e9426369409bc434b9825d8ac15978ee
anton.ihnatovich <anton.ihnatovich>
2018-09-04 10:49:30 +0300

    implemented add todo presenter

--- TODO Lite/TODO Lite/modules/add/presenter/AddTodoPresenter.swift
+++ TODO Lite/TODO Lite/modules/add/presenter/AddTodoPresenter.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import UIKit
+
+class AddTodoPresenter: AddTodoPresenterProtocol {
+    var view: AddTodoViewProtocol?
+    var interactor: AddTodoInteractorInputProtocol?
+    var router: AddTodoRouterProtocol?
+    
+    init(view: AddTodoViewProtocol?, interactor: AddTodoInteractorInputProtocol, router: AddTodoRouterProtocol) {
+        self.view = view
+        self.interactor = interactor
+        self.router = router
+    }
+    
+    func addTodo(with title: String?, and category: String?) {
+        
+        guard let title = title, !title.isEmpty else {
+            view?.showError(for: .title)
+            return
+        }
+        guard let category = category, !category.isEmpty else {
+            view?.showError(for: .categoy)
+            return
+        }
+        
+        interactor?.addTodo(with: title, and: category)
+    }
+}
+
+// MARK: AddTodoInteractorOutputProtocol
+
+extension AddTodoPresenter: AddTodoInteractorOutputProtocol {
+    
+    func didAddTodo() {
+        view?.dismiss()
+    }
+}
--- TODO Lite/TODO Lite/modules/add/presenter/AddTodoPresenterProtocol.swift
+++ TODO Lite/TODO Lite/modules/add/presenter/AddTodoPresenterProtocol.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+protocol AddTodoPresenterProtocol: class {
+    var view: AddTodoViewProtocol? { get set }
+    var interactor: AddTodoInteractorInputProtocol? { get set }
+    var router: AddTodoRouterProtocol? { get set }
+    
+    func addTodo(with title: String?, and category: String?)
+    func didAddTodo()
+}
